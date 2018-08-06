e79e881a83c16a86e9a744b7fa9ec514e3b0b746
kavysv <kavysv@DIN73003010.corp.capgemini.com>
2018-08-06 10:53:14 +0530

    updated

--- Day1Git/src/main/java/org/cap/demo/MainClass.java
+++ Day1Git/src/main/java/org/cap/demo/MainClass.java
@@ -4,9 +4,13 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+		
+		
 greet();
 	}
 public  static void greet() {
 	System.out.println("hi");
-}
+	for(int i=1;i<3;i++) {
+	System.out.println("kavya");
+}}
 }
